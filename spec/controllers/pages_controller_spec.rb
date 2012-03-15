bf73d8ff21555ea2a2b5636888845fa1ea718364
Rick Edwards <rob@ThinkPad-T61.(none)>
2012-03-15 14:18:28 -0400

    Fixed test fail issue

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -25,7 +25,23 @@ describe PagesController do
     end
   end
 
-describe "GET 'about'" do
+ describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Help")
+    end
+    it "should have a non-blank body" do
+      get 'help'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+end
+
+ describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
@@ -33,11 +49,15 @@ describe "GET 'about'" do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                  :content => "#{@base_title} | About")
+                                    :content => "#{@base_title} | About")
+    end
+    it "should have a non-blank body" do
+      get 'about'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
-  describe "GET 'contact'" do
+    describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
@@ -45,18 +65,11 @@ describe "GET 'about'" do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                  :content => "#{@base_title} | Contact")
+                                    :content => "#{@base_title} | Contact")
     end
-  end
-describe "GET 'help'" do
-    it "should be successful" do
-      get 'help'
-      response.should be_success
-    end
-    it "should have the right title" do
-      get 'help'
-      response.should have_selector("title",
-                  :content => "#{@base_title} | Help")
-    end
-  end
+    it "should have a non-blank body" do
+      get 'contact'
+      response.body.should_not =~ /<body>\s*<\/body>/
+     end
+end
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
@@ -10,7 +9,7 @@ describe "LayoutLinks" do
     get '/contact'
     response.should have_selector('title', :content => "Contact")
  end
-  it "should have an About  page at '/about'" do
+  it "should have a About page at '/about'" do
     get '/about'
     response.should have_selector('title', :content => "About")
  end
@@ -18,5 +17,4 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
  end
-  
 end
